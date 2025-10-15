4c877f2b6771cf8ebb51b557ddfa1f398fa88c62
Robson Borges <57491372+robson1622@users.noreply.github.com>
2025-10-15 14:30:08 -0300

    Main atualizada

--- prod_and_inverse_prod/main.py
+++ prod_and_inverse_prod/main.py
@@ -1,28 +1,49 @@
 from inverseProdOfTwoVariables import InverseProdOfTwoVariables
 from prodOfTwoVariables import ProdOfNormalRVs
+from Tabela import Tabela,Parametros
+import threading
+import time
 
-if __name__ == "__main__":
-    #================== Inverse ==========================
-    muX_inv = 20.0        
-    sigmaX_inv = 30.0     
-    muY_inv = 300.0      
-    sigmaY_inv = 150.0    
-    target_p_inv = 0.95   
-    
+def processoA_thread(parametro : Parametros):
+    muX_inv = parametro.mux1     
+    sigmaX_inv = parametro.sigmax1 
+    muY_inv = parametro.muy    
+    sigmaY_inv = parametro.sigmay
+    target_p_inv = 0.98
+
     try:
-        print("f\n ---------- InverseProdOfTwoVariables -----------\n")
+        # print("f\n ---------- InverseProdOfTwoVariables -----------\n")
         solver = InverseProdOfTwoVariables(muX_inv, sigmaX_inv, muY_inv, sigmaY_inv, target_p_inv)
-      
+    
         c_solution, mc_p = solver.solve_inverse_cdf(n_samples=5000000)
 
-        solver._print_verification_results()
+        # solver._print_verification_results()
 
-        solver.plot_cdf()
-        
+        tabela.salvarColunaSaida(valor=c_solution,parametro=i,linha_lwb=0)
+        # solver.plot_cdf()
+            
     except Exception as e:
         print(f"\nErro: {e}")
 
 
+if __name__ == "__main__":
+    THREAD_MODE = True
+    #================== Inverse ==========================
+
+    tabela = Tabela(pagina='Matlab_Data',localENome='Stock_Data_in_days_cv_0,2_5V.xlsx')
+    parametros = tabela.pegarParametros()
+
+
+    for i in parametros :
+        if (i.mux1 > 0) :
+            if THREAD_MODE :
+                threading.Thread(target=processoA_thread, args=(i,)).start()
+            else :
+                processoA_thread(parametro=i)
+            
+        
+
+    exit(0)
     #============== Prod ==================
     muX_prod = 100.0        
     sigmaX_prod = 35.8305     
