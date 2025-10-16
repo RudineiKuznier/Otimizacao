583299191b491701e6cdc1864a119e97189053a7
RuKuznierS <rudineikuzniersilva@gmail.com>
2025-10-16 10:23:35 -0300

    remove parametro desnecessário

--- Prod_Of_Two_Variables/main.py
+++ Prod_Of_Two_Variables/main.py
@@ -6,21 +6,18 @@ if __name__ == "__main__":
     sigmaX = 16.8196    
     muY = 13.11      
     sigmaY = 0.4786   
-
-    n_samples_mc = 1000000 
     
     try:
-     
         analyzer = ProdOfNormalRVs(muX, sigmaX, muY, sigmaY)
 
-        std_dev, probability = analyzer.solve_cdf(n_samples=n_samples_mc)
+        std_dev, probability = analyzer.solve_cdf()
         print(f"{std_dev} -- {probability}")
         #analyzer._print_verification_results()
         #analyzer.plot_cdfs()
         
 
         analyzer2 = ProdOfNormalRVs(muX, sigmaX, muY, sigmaY, 2000)
-        std_dev, probability = analyzer2.solve_cdf(n_samples=n_samples_mc)
+        std_dev, probability = analyzer2.solve_cdf()
         print(f"{std_dev} -- {probability}")
         #analyzer._print_verification_results()
         #analyzer.plot_cdfs()
